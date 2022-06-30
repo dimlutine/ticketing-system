b8eb2f7521556141c0f3a8a82e14833fc226ac59
Jeremy Rowley <dimlutine@gmail.com>
2022-06-30 07:38:27 -0400

    add ticket model and routes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the API' });
 });
 
+// Routes
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
 app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
